21ade775a20da9603246944089c54e9da2e1ddf5
Jerzy Listewnik <pzx102011@student.wsb.poznan.pl>
2024-01-03 23:22:00 +0100

    Login form and controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthenticationController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get(
-    '/',
-    function () {
-        return view('welcome');
-    }
-);
+Route::get('/', fn () => view('welcome'));
+Route::get('/login', [AuthenticationController::class, 'showLoginForm']);
+Route::post('/authenticate', [AuthenticationController::class, 'authenticate']);
