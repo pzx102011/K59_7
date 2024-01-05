f02ab6440f9147aa5d473ee0d2260609b9b5c9b2
Jakub Lewandowski <pzx100025@student.wsb.poznan.pl>
2024-01-06 00:38:07 +0100

    Dashboard views improved

--- resources/views/dashboard/main.blade.php
+++ resources/views/dashboard/main.blade.php
@@ -2,24 +2,44 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="login-box">
-        <div class="login-logo">
-            <a href="{{ url('/') }}">{{ config('app.name', 'Laravel') }}</a>
-        </div>
+    <section class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
 
-        <div class="card">
-            <div class="card-header">
-                <h1>Jestę daszbordę</h1>
-            </div>
-            <div class="card-body">
-                <div class="mb-3 row">
-                    @if($user->hasRole(UserRoleEnum::Administrator))
-                        <a href="{{route('users.index')}}" class="btn btn-primary btn-sm">Zarządzaj
-                            użytkownikami</a>
-                    @endif
+                    <div class="login-logo">
+                        <a href="{{ url('/') }}">{{ config('app.name', 'Laravel') }}</a>
+                    </div>
+
+                    <section class="content">
+                        <div class="container-fluid">
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="card">
+                                        <div class="card-header">
+                                            <h3 id="mail-table" class="card-title">Dashboard</h3>
+                                        </div>
+
+                                        <div class="card-body">
+                                            <div class="row">
+                                                <div class="col-md">
+
+                                                    <div class="text-center">
+                                                    <img src="https://cdn.pixabay.com/photo/2012/05/07/01/54/owl-47526_1280.png"
+                                                         style="max-height: 50vh; max-width: 100%; height: auto;"
+                                                    />
+                                                    </div>
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
                 </div>
             </div>
         </div>
-    </div>
-    <!-- /.login-box -->
+    </section>
 @endsection
